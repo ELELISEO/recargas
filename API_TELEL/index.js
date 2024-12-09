5569259522fee5e1a0100a2662a073dea9b54abe
ELELISEO <jcolunga09@alumnos.uaq.mx>
2024-12-09 01:31:48 -0600

    first commit

---
+++ API_TELEL/index.js
@@ -0,0 +1,17 @@
+// Importar Express
+const express = require('express');
+const app = express();
+
+// Middleware para parsear JSON
+app.use(express.json());
+
+// Ruta base
+app.get('/', (req, res) => {
+  res.send('¡Bienvenido a mi API!');
+});
+
+// Puerto del servidor
+const PORT = 3002;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
